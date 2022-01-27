37a3f8a6f608af595ef1b0ea6098ead8b7533652
Michal Zbiegień <77878599+ndiyathanda@users.noreply.github.com>
2022-01-27 14:03:33 +0100

    chuj

---
+++ bot/a.py
@@ -0,0 +1,8 @@
+import pyautogui, sys, keyboard
+
+while True:
+    if keyboard.is_pressed("w"):
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
