2108c0aa5c8bcbdadfcb3be88e928b212b9d5ecb
Michal Zbiegień <77878599+ndiyathanda@users.noreply.github.com>
2022-01-28 12:31:47 +0100

    Delete bot directory

--- bot/a.py
+++
@@ -1,8 +0,0 @@
-import pyautogui, sys, keyboard
-
-while True:
-    if keyboard.is_pressed("w"):
-        x, y = pyautogui.position()
-        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-        print(positionStr, end='')
-        print('\b' * len(positionStr), end='', flush=True)
--- bot/elo.bat
+++
@@ -1,2 +0,0 @@
-python C:\Users\michal\Desktop\jiodwc\main.py
-pause
